ba92b18163f6b0da8bd2b4bc569123182d638733
Bora Durmuş <75389478+Boradurmus1@users.noreply.github.com>
2021-02-05 02:19:45 +0300

    Sonun Başlangıcı

---
+++ Tubitak-JavaScript-Deneme/filtreler/tritanopia.js
@@ -0,0 +1,21 @@
+//Filtrelerin üst üste binmmesini önlemek için eğer varsa daha önceden seçili olan filtrenin kaldırılması sağlanmıştır
+try {
+    if (document.getElementById("stil_kimligi")) {
+        stilID = document.getElementById("stil_kimligi").remove();
+        filtreID = document.getElementById("filtre_kimligi").remove();
+    }
+} catch(e) {
+    console.log(e);
+}
+//Sekmeye uygulanacak filtrenin CSS kodu için gerekli tanımlamalar HTML etiketleriyle yapılmıştır
+filtreID = document.createElement('div');
+filtreID.id = "filtre_kimligi";
+filtreID.setAttribute('style', 'height: 0; padding: 0; margin: 0; line-height: 0;');
+document.body.appendChild(filtreID);
+stilID = document.createElement('style');
+stilID.id = "stil_kimligi";
+document.body.appendChild(stilID);
+
+//Sekmeye uygulanacak filtrenin CSS kodu
+filtreID.innerHTML = '<svg id="colorblind-filters" style="display: none"> <defs> <filter id="Tritanopia"> <feColorMatrix type="matrix" values="1,0,-0.2,0,0 0,1,0,0,0 -0.4,0,1,-1.1,0 0,0,0,1,0" in="SourceGraphic" /> </filter> </defs> </svg>';
+stilID.innerHTML = 'html{-webkit-filter:url(#Tritanopia);-moz-filter:(#Tritanopia);-ms-filter:(#Tritanopia);-o-filter:(#Tritanopia);filter:(#Tritanopia);}'
